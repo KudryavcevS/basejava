f9dfb21cf415bc102b04ca5682f24f3462b5ddac
turbolos <turrbolos@gmail.com>
2018-11-17 18:16:38 +0300

    first try, without test

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
-/**
- * Array based storage for Resumes
- */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int currentSize = size();
+        for (int i = 0; i < currentSize; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                storage[i] = storage[currentSize - 1];
+                storage[currentSize - 1] = null;
+            }
+        }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
+    Resume[] getAll(){
+        Resume[] allResume = new Resume[size()];
+        for (int i = 0; i < allResume.length; i++) {
+            allResume[i] = storage[i];
+        }
+        return allResume;
     }
 
     int size() {
-        return 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
+        return storage.length;
     }
 }
