b7bdd0f8ae70d856a9fd6d178e6a3e01f2aa0d00
turbolos <turrbolos@gmail.com>
2018-11-21 23:25:26 +0300

    build Reflection

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -1,8 +1,12 @@
+import model.Resume;
+
+import java.lang.reflect.Method;
+
 public class MainReflection {
-    public static void main (String[] args){
+    public static void main(String[] args) throws Exception {
         Resume r = new Resume("resume from reflection");
         Method[] methods = r.getClass().getMethods();
-        for (Method method : methods){
+        for (Method method : methods) {
             if (method.getName().equals("toString")) System.out.println(method.invoke(r));
         }
     }
